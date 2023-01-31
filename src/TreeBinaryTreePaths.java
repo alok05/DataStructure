0df1cf16358834c9e1962121ace5a6c6ddb7112e
alok05 <alokyadav0507@gmail.com>
2023-01-31 20:18:49 +0000

    Data structures

--- src/TreeBinaryTreePaths.java
+++ src/TreeBinaryTreePaths.java
@@ -14,49 +14,51 @@
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-public class TreeBinaryTreePaths {
-    public List<String> binaryTreePaths(TreeNode root) {
-        List<String> result = new ArrayList<String>();
 
+
+  class TreeNode1 {
+     int val;
+      TreeNode1 left;
+      TreeNode1 right;
+      TreeNode1(int x) { val = x; }
+ }
+
+public class TreeBinaryTreePaths {
+    TreeNode1 root;
+    public List<String> binaryTreePaths() {
+        List<String> result = new ArrayList<>();
+        
         if(root == null) {
             return result;
         }
-
+        
         helper(new String(), root, result);
-
+        
         return result;
     }
-
-    public void helper(String current, TreeNode root, List<String> result) {
+    
+    public void helper(String current, TreeNode1 root, List<String> result) {
         if(root.left == null && root.right == null) {
-            result.add(current + root.data);
+            result.add(current + root.val);
         }
 
         if(root.left != null) {
-            helper(current + root.data + "->", root.left, result);
+            helper(current + root.val + "->", root.left, result);
         }
 
         if(root.right != null) {
-            helper(current + root.data + "->", root.right, result);
+            helper(current + root.val + "->", root.right, result);
         }
     }
     public static void main(String[] args) {
         TreeBinaryTreePaths tree = new TreeBinaryTreePaths();
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(3);
-        root.left.right = new TreeNode(5);
+        tree.root = new TreeNode1(1);
+        tree.root.left = new TreeNode1(2);
+        tree.root.right = new TreeNode1(3);
+        tree.root.left.right = new TreeNode1(5);
 
         System.out.println(
-            "Binary tree paths are "+tree.binaryTreePaths(root));
+            "Preorder traversal of binary tree is "+tree.binaryTreePaths());
+//        tree.printPreorder();
     }
 }
